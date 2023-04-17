7bf55c96d3f56bb31909c597f8562361ba1cbb95
rayanmassri20 <rayan.massri@campus.technion.ac.il>
2023-04-17 19:17:28 +0300

    fixing_bug

--- print_number.c
+++ print_number.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
+
 int main() {
-	for(int i = 6; i > 0; i--) {
-		printf("%d", course_number[i]);
+int course_number[6] = {4, 2, 1, 4, 3, 2};
+
+	for(int i = 5; i >= 0; i--) {
+	  printf("%f", course _number[i]);
 	}
 	printf("\n");
 	return 0;
